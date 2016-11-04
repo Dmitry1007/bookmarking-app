011d45589f73bdbf93f09048a561fcde63458790
Dmitry Sky <dmitry.sky@icloud.com>
2016-11-04 16:42:31 -0600

    build out App.vue

---
+++ app/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  <sidebar
+    :categories="categories"
+    v-on:category-selected="setSelectedCategory">
+    <!-- bind 'selected-category event to the event handler setSelectedCategory' -->
+  </sidebar>
+  <bookmark-list
+    :bookmarks="bookmarks | filterByCategory selectedCategory"
+    :categories="categories">
+  </bookmark-list>
+  </div>
+</template>
+
+<script>
+// the JS for our component
+// We will export a Vue component options object here
+</script>
